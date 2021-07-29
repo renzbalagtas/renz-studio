8ee1f51158e5c0deb56508319320815de897c2e8
renzbalagtas <renzmbalagtas@gmail.com>
2021-07-29 10:29:54 -0400

    Enter

--- random_yt.js
+++ random_yt.js
@@ -8,7 +8,8 @@ const videos = ["https://www.youtube.com/embed/IWYImJiFHd4", "https://www.youtub
             "https://www.youtube.com/embed/9gF2UySGZAU", "https://www.youtube.com/embed/kW6z5TyU8QA",
             "https://www.youtube.com/embed/f2jqzVurDuY", "https://www.youtube.com/embed/XSHf4sivlWA",
             "https://www.youtube.com/embed/GB6BERGTTsA", "https://www.youtube.com/embed/wTndtUTFqb8",
-            "https://www.youtube.com/embed/ODejBuFlY20", "https://www.youtube.com/embed/e_PMcFjE9as"];
+            "https://www.youtube.com/embed/ODejBuFlY20", "https://www.youtube.com/embed/e_PMcFjE9as",
+            "https://www.youtube.com/embed/lgOnwN0cIII"];
 
             window.onload = function () {
                 const playerIframe = document.getElementById("player");
