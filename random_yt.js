75812d83ed23568333c6ef211bd51fb6248e09b2
Daniel Cowman <32685899+permadeath@users.noreply.github.com>
2022-05-03 09:19:02 -0400

    Added a missing comma in the videos list
    I'm an idiot.

--- random_yt.js
+++ random_yt.js
@@ -2,7 +2,7 @@ const videos = ["https://www.youtube.com/embed/IWYImJiFHd4", "https://www.youtub
                 "https://www.youtube.com/embed/am6ngIjzeao", "https://www.youtube.com/embed/64LlQau30tI", 
                 "https://www.youtube.com/embed/agzL-VwSJac", "https://www.youtube.com/embed/T-eVg4dZErI", 
                 "https://www.youtube.com/embed/dysOLygI8ZE", "https://www.youtube.com/embed/W55x5OmNDU4?start=98",
-                "https://www.youtube.com/embed/68eue5cpbsE", "https://www.youtube.com/embed/NKoJDyKo1QQ"
+                "https://www.youtube.com/embed/68eue5cpbsE", "https://www.youtube.com/embed/NKoJDyKo1QQ",
                 "https://www.youtube.com/embed/qfRlmwjD2Fs", "https://www.youtube.com/embed/BZGY81KJJPU?start=8",
                 "https://www.youtube.com/embed/kW6z5TyU8QA", "https://www.youtube.com/embed/f2jqzVurDuY",
                 "https://www.youtube.com/embed/XSHf4sivlWA", "https://www.youtube.com/embed/GB6BERGTTsA",
