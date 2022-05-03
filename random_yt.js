b43b549e3c3a4f0a759210c68189bd784e6465e1
Daniel Cowman <32685899+permadeath@users.noreply.github.com>
2022-05-03 00:55:32 -0400

    Add 2 important videos
    I added 2 very important video links to the videos list.

--- random_yt.js
+++ random_yt.js
@@ -1,12 +1,13 @@
 const videos = ["https://www.youtube.com/embed/IWYImJiFHd4", "https://www.youtube.com/embed/ReBzqzztRNQ?start=44",
-            "https://www.youtube.com/embed/am6ngIjzeao", "https://www.youtube.com/embed/64LlQau30tI", 
-            "https://www.youtube.com/embed/agzL-VwSJac", "https://www.youtube.com/embed/T-eVg4dZErI", 
-            "https://www.youtube.com/embed/dysOLygI8ZE", "https://www.youtube.com/embed/W55x5OmNDU4?start=98",
-            "https://www.youtube.com/embed/68eue5cpbsE", "https://www.youtube.com/embed/NKoJDyKo1QQ"
-            "https://www.youtube.com/embed/qfRlmwjD2Fs", "https://www.youtube.com/embed/BZGY81KJJPU?start=8",
-            "https://www.youtube.com/embed/kW6z5TyU8QA", "https://www.youtube.com/embed/f2jqzVurDuY",
-            "https://www.youtube.com/embed/XSHf4sivlWA", "https://www.youtube.com/embed/GB6BERGTTsA",
-            "https://www.youtube.com/embed/wTndtUTFqb8", "https://www.youtube.com/embed/ODejBuFlY20"];
+                "https://www.youtube.com/embed/am6ngIjzeao", "https://www.youtube.com/embed/64LlQau30tI", 
+                "https://www.youtube.com/embed/agzL-VwSJac", "https://www.youtube.com/embed/T-eVg4dZErI", 
+                "https://www.youtube.com/embed/dysOLygI8ZE", "https://www.youtube.com/embed/W55x5OmNDU4?start=98",
+                "https://www.youtube.com/embed/68eue5cpbsE", "https://www.youtube.com/embed/NKoJDyKo1QQ"
+                "https://www.youtube.com/embed/qfRlmwjD2Fs", "https://www.youtube.com/embed/BZGY81KJJPU?start=8",
+                "https://www.youtube.com/embed/kW6z5TyU8QA", "https://www.youtube.com/embed/f2jqzVurDuY",
+                "https://www.youtube.com/embed/XSHf4sivlWA", "https://www.youtube.com/embed/GB6BERGTTsA",
+                "https://www.youtube.com/embed/wTndtUTFqb8", "https://www.youtube.com/embed/ODejBuFlY20",
+                "https://www.youtube.com/embed/LTE5CY6NTAk", "https://www.youtube.com/embed/rwkCEsMpA3E"];
 
             window.onload = function () {
                 const playerIframe = document.getElementById("player");
